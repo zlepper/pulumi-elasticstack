8ea6ffa05d4ae0e3fd2f73006ce7d0ae37e46cec
zlepper <rhdh@digizuite.com>
2023-01-20 09:32:08 +0100

    step 1

--- examples/go.mod
+++ examples/go.mod
@@ -1,3 +1,3 @@
-module github.com/pulumi/pulumi-xyz/examples
+module github.com/zlepper/pulumi-elasticstack/examples
 
 go 1.16
--- provider/cmd/pulumi-resource-elasticstack/pulumi-resource-elasticstack/main.go
+++ provider/cmd/pulumi-resource-elasticstack/pulumi-resource-elasticstack/main.go
@@ -20,8 +20,8 @@ import (
 	_ "embed"
 
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
-	xyz "github.com/pulumi/pulumi-xyz/provider"
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
+	elasticstack "github.com/zlepper/pulumi-elasticstack/provider"
+	"github.com/zlepper/pulumi-elasticstack/provider/pkg/version"
 )
 
 //go:embed schema-embed.json
@@ -29,5 +29,5 @@ var pulumiSchema []byte
 
 func main() {
 	// Modify the path to point to the new provider
-	tfbridge.Main("xyz", version.Version, xyz.Provider(), pulumiSchema)
+	tfbridge.Main("elasticstack", version.Version, elasticstack.Provider(), pulumiSchema)
 }
